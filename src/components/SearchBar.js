1b795a2b728a17fcf345ba2d48e15bb3a2298852
Jonathan King <jonpking@gmail.com>
2021-02-16 13:32:43 -0700

    set up SearchBar to hit youtube api and return results

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,8 +12,7 @@ class SearchBar extends React.Component {
 
     onFormSubmit = event => {
         event.preventDefault();
-
-        //make sure to call callback from parent component
+        this.props.onFormSubmit(this.state.term);
     };
 
     render() {
